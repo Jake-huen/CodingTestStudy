fe5249e9ad87b61a88d67896421a72b530eb2a32
tae77777 <tae77777@naver.com>
2021-04-14 13:49:10 +0900

    4/14 algorithm

---
+++ Algorithm/DFS_BFS/14502.py
@@ -0,0 +1,17 @@
+n,m = map(int,input().split())
+graph=[]
+for i in range(n):
+    graph.append(list(map(int,input().split())))
+#브루트포스로 1로 만들곳 2개 선택
+#bfs로 감염지역 싹 덮고
+#for문으로 안전 영역 확인
+def bfs():
+
+def bback(cnt):
+    if cnt==3:
+        bfs()
+        return
+    for i in range(n):
+        for j in range(m):
+            if
+
--- Algorithm/DFS_BFS/1697.py
+++ Algorithm/DFS_BFS/1697.py
@@ -1,1 +1,18 @@
-n,k = map(int,input().split())
+from collections import deque
+n, k = map(int, input().split())
+# n에서 k로 가기
+d=[0]*100001
+def bfs(n):
+    queue=deque()
+    queue.append(n)
+    while queue:
+        v=queue.popleft()
+        if v==k:
+            print(d[v])
+            return
+        for next in (v-1,v+1,v*2):
+            if 0<=next<100001 and not d[next]:
+                d[next]=d[v]+1
+                queue.append(next)
+
+bfs(n)
