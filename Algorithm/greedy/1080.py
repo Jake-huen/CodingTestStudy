5e7a76d56d1c707bddec02edcced59e3564560de
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2021-11-15 12:12:54 +0900

    1080 백준

--- Algorithm/greedy/1080.py
+++ Algorithm/greedy/1080.py
@@ -2,13 +2,14 @@
 n,m=map(int,input().split())
 a=[]
 b=[]
-def reverse(x):
-    for i in range(3):
-        for j in range(3):
-            if x[i][j]==0:
-                x[i][j]=1
-            elif x[i][j]==1:
-                x[i][j]=0
+cnt=0
+def reverse(arr,x,y):
+    for i in range(x,x+3):
+        for j in range(y,y+3):
+            if arr[i][j]==0:
+                arr[i][j]=1
+            elif arr[i][j]==1:
+                arr[i][j]=0
 def same(x,y):
     for i in range(n):
         for j in range(m):
@@ -19,3 +20,14 @@ for _ in range(n):
     a.append(list(map(int,input())))
 for _ in range(n):
     b.append(list(map(int,input())))
+for i in range(n-2):
+    for j in range(m-2):
+        if a[i][j]!=b[i][j]:
+            reverse(a,i,j)
+            cnt+=1
+        else:
+            continue
+if same(a,b):
+    print(cnt)
+else:
+    print(-1)
