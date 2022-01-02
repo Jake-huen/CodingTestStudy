6cb64c4ea5874353db84cf3a5cb474b09f3493c3
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-01-02 13:05:18 +0900

    1158 해결

--- Algorithm/dataStructure/1158.py
+++ Algorithm/dataStructure/1158.py
@@ -1,3 +1,17 @@
-#(n,k) 요세푸스 순열
+#(n,k) - 요세푸스 순열
+from collections import deque
 n,k = map(int,input().split())
-
+answer = []
+q1=deque([])
+for i in range(n):
+    q1.append(i+1)
+for _ in range(n):
+    for _ in range(k-1):
+        num = q1.popleft()
+        q1.append(num)
+    num = q1.popleft()
+    answer.append(num)
+print("<",end='')
+for i in range(n-1):
+    print(answer[i],end=', ')
+print(answer[n-1],end='>')
