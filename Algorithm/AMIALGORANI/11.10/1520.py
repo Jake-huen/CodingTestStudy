c7a61e79ac8800d356ce736aad1f0cc8807a55c0
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-11-07 16:52:53 +0900

    11/7 1520

---
+++ Algorithm/AMIALGORANI/11.10/1520.py
@@ -0,0 +1,28 @@
+from heapq import heappush,heappop
+import sys
+input = sys.stdin.readline
+
+m,n = map(int,input().split())
+graph=[]
+visited=[[0 for _ in range(n)] for _ in range(m)]
+for _ in range(m):
+    graph.append(list(map(int,input().split())))
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+q=[(0,0,0)]
+visited[0][0]=1
+
+while q:
+    count,x,y = heappop(q)
+    current = graph[x][y] # 현재 값
+    for i in range(4):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        if not 0<=nx<m or not 0<=ny<n:
+            continue
+        if graph[nx][ny]>=graph[x][y]:
+            continue
+        if visited[nx][ny]==0:
+            heappush(q, (-graph[nx][ny], nx, ny)) # 더 값 높은 곳 방문하기
+        visited[nx][ny] += visited[x][y] #방문한 곳, 방문안한곳 둘 다 더해주기
+print(visited[m-1][n-1])
