df91f683cc179dd6765ca8a9f7e1cc517b0a3ce8
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-01-05 10:28:00 +0900

    17609 주석 추가

--- Algorithm/AMIALGORANI/1.05/17609.py
+++ Algorithm/AMIALGORANI/1.05/17609.py
@@ -1,33 +1,33 @@
 import sys
-
 input = sys.stdin.readline
-
 T = int(input())
 letterlist = []
-
-
-def palindrom(letter):
+answers = []
+for i in range(T):
+    letter = list(input())
+    letterlist.append(letter[:-1])
+for letter in letterlist:
     left = 0
     right = len(letter) - 1
+    answer = 2
     while True:
         if letter[left] == letter[right]:
-            left += 1
-            right -= 1
-            if left >= right:
+            letter.pop(right)
+            letter.pop(left)
+            right = len(letter) - 1
+            if len(letter) == 0 or len(letter) == 1:
+                answer = 0
                 break
-        elif letter[left] != letter[right]:  # 다른 경우
-            if left < right - 1:
-                temp = letter[:right] + letter[right + 1:]
-                if temp[:] == temp[::-1]:
-                    return 1
-            if left + 1 < right:
-                temp = letter[:left] + letter[left + 1:]
-                if temp[:] == temp[::-1]:
-                    return 1
-            return 2
-    return 0
-
-
-for i in range(T):
-    letter = list(input())[:-1]
-    print(palindrom(letter))
+        elif letter[left] != letter[right]: # 다른 경우
+            temp1 = letter[left + 1:right + 1]
+            if temp1[:] == temp1[::-1]:
+                answer = 1
+                break
+            else:
+                temp2 = letter[left:right]
+                if temp2[:] == temp2[::-1]:
+                    answer = 1
+                break
+    answers.append(answer)
+for answer in answers:
+    print(answer)
