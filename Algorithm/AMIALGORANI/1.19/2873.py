506169648c41c3b862ebf7ea169ad6a314f2b6a4
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-01-27 13:47:18 +0900

    2873 수정

--- Algorithm/AMIALGORANI/1.19/2873.py
+++ Algorithm/AMIALGORANI/1.19/2873.py
@@ -15,52 +15,49 @@ def minGraph():
     for i in range(r):
         for j in range(c):
             if (i + j) % 2 == 1:
-                if graph[i][j] <= ans:
+                if graph[i][j] < ans:
                     ans = graph[i][j]
                     x = i
                     y = j
     return x, y
 
 
-def happy(r, c):
-    if (r % 2 == 1 and c % 2 == 1) or (r % 2 == 1 and c % 2 == 0):
-        answer = ('R' * (c - 1) + 'D' + 'L' * (c - 1) + 'D') * (r // 2) + 'R' * (c - 1)
-        return answer
-    elif r % 2 == 0 and c % 2 == 1:
-        answer = ('D' * (r - 1) + 'R' + 'U' * (r - 1) + 'R') * (c // 2) + 'D' * (r - 1)
-        return answer
-    else:
-        a, b = minGraph()  # 못가는곳
-        check = False
-        answer = ''
-        for i in range(0, r, 2):
-            if not check:
-                if a == i:
-                    for j in range(1, c, 2):
-                        if b == j:
-                            answer += 'DR'
-                            check = True
-                        else:
-                            if not check:
-                                answer += 'DRUR'
-                            else:
-                                answer += 'RURD'
-                    answer += 'D'
-                elif a == i + 1:
-                    for j in range(0, c, 2):
-                        if b == j:
-                            answer += 'RD'
-                        else:
-                            if not check:
-                                answer += 'DRUR'
-                            else:
-                                answer += 'RDRU'
-                    answer += 'D'
-                else:
-                    answer += ('R' * (c - 1) + 'D' + 'L' * (c - 1) + 'D')
-            else:
-                answer += ('L' * (c - 1) + 'D' + 'R' * (c - 1) + 'D')
-        return answer[:-1]
+if r % 2 == 1:
+    answer = ('R' * (c - 1) + 'D' + 'L' * (c - 1) + 'D') * (r // 2) + 'R' * (c - 1)
+    print(answer)
+elif c % 2 == 1:
+    answer = ('D' * (r - 1) + 'R' + 'U' * (r - 1) + 'R') * (c // 2) + 'D' * (r - 1)
+    print(answer)
+else:
+    low = 1000
+    position = [-1, -1]
+    for i in range(r):
+        if i % 2 == 0:
+            for j in range(1, c, 2):
+                if low > graph[i][j]:
+                    low = graph[i][j]
+                    position = [i, j]
+        else:  # i % 2 == 1
+            for j in range(0, c, 2):
+                if low > graph[i][j]:
+                    low = graph[i][j]
+                    position = [i, j]
 
+    res = ('D' * (r - 1) + 'R' + 'U' * (r - 1) + 'R') * (position[1] // 2)
+    x = 2 * (position[1] // 2)
+    y = 0
+    xbound = 2 * (position[1] // 2) + 1
+    while x != xbound or y != r - 1:
+        if x < xbound and [y, xbound] != position:
+            x += 1
+            res += 'R'
+        elif x == xbound and [y, xbound - 1] != position:
+            x -= 1
+            res += 'L'
+        if y != r - 1:
+            y += 1
+            res += 'D'
 
-print(happy(r, c))
+    res += ('R' + 'U' * (r - 1) + 'R' + 'D' * (r - 1)) * ((c - position[1] - 1) // 2)
+
+    print(res)
