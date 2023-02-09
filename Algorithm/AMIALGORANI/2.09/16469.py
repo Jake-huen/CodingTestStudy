6eb21c15003a4baed8b0bb0ef335dd67cb6c99db
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-02-09 18:13:57 +0900

    16469 수정

--- Algorithm/AMIALGORANI/2.09/16469.py
+++ Algorithm/AMIALGORANI/2.09/16469.py
@@ -10,6 +10,9 @@ for _ in range(3):
     x, y = map(int, input().split())
     akdang.append([x - 1, y - 1])  # 처음 악당들 위치
 
+
+# row가 X, col이 Y
+
 def possible_graph(start):
     q = deque()
     q.append(start)
@@ -18,37 +21,46 @@ def possible_graph(start):
     new_graph = [[-1 for _ in range(c)] for _ in range(r)]  # 출발지에서 목표지까지의 거리
     new_graph[start[0]][start[1]] = 0
     while q:
-        x, y = q.pop()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        start_x, start_y = q.popleft()
+        for idx in range(4):
+            nx = start_x + dx[idx]
+            ny = start_y + dy[idx]
             if 0 <= nx < r and 0 <= ny < c:
                 if graph[nx][ny] == '0' and new_graph[nx][ny] == -1:  # 지나갈 수 있는 길이고 아직 방문하지 않은 곳
                     q.append([nx, ny])
-                    new_graph[nx][ny] = new_graph[x][y] + 1
+                    new_graph[nx][ny] = new_graph[start_x][start_y] + 1
     return new_graph
 
 
 first = possible_graph(akdang[0])
 second = possible_graph(akdang[1])
 third = possible_graph(akdang[2])
-print(first)
-print(second)
-print(third)
+
 answer = []
 for i in range(r):
     for j in range(c):
-        if first[i][j] != -1 and second[i][j] != -1 and third[i][j] != -1:  # 전부 다 일단 방문한 곳
+        if first[i][j] > -1 and second[i][j] > -1 and third[i][j] > -1:  # 전부 다 일단 방문한 곳
             answer.append(max(first[i][j], second[i][j], third[i][j]))
-print(answer)
+
 if len(answer) == 0:
     print(-1)
 else:
     ans = min(answer)
-    ans2 = 0 # ans와 같은 개수
+    ans2 = 0  # ans와 같은 개수
     for temp in answer:
         if temp == ans:
             ans2 += 1
-    print(min(answer))
+    print(ans)
     print(ans2)
 
+'''
+5 6
+011110
+100000
+100000
+110000
+110110
+1 1
+2 2
+3 3
+'''
