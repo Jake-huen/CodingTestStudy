dd15c387b2a4baaeff6d19a63e3d65d9838c9fcf
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-02-09 09:42:54 +0900

    [Gold III] Title: Messi Gimossi, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/17297. Messi Gimossi/Messi Gimossi.py
+++ 백준/Gold/17297. Messi Gimossi/Messi Gimossi.py
@@ -1,21 +1,22 @@
-import sys
-
-
-def find(idx, M, dp):
+def Messi(idx, m):
     if idx <= 1:
-        return "Messi Gimossi"[M - 1]
-    if M > dp[idx - 1] + 1:
-        res = find(idx - 2, M - dp[idx - 1] - 1, dp)
-    elif M < dp[idx - 1]:
-        res = find(idx - 1, M, dp)
+        return "Messi Gimossi"[m - 1]
+    if m > dp[idx - 1] + 1:
+        return Messi(idx - 2, m - dp[idx - 1] - 1)
+    elif m < dp[idx - 1]:
+        return Messi(idx - 1, m)
     else:
         return " "
-    return res
 
 
-M = int(sys.stdin.readline())
+m = int(input())
 dp = [5, 13]
-while dp[-1] < M:
-    dp.append(dp[-2] + 1 + dp[-1])
-res = find(len(dp) - 1, M, dp)
-print("Messi Messi Gimossi" if res == " " else res)
+while True:
+    if dp[-1] >= m:
+        break
+    dp.append(dp[-1] + dp[-2] + 1)
+ans = Messi(len(dp) - 1, m)
+if ans == " ":
+    print("Messi Messi Gimossi")
+else:
+    print(ans)
