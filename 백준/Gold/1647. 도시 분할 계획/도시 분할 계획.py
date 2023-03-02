62f3522e9fa890ec93f70fb42a0d5b81430f7585
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-03-02 13:47:01 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 5456 ms, Memory: 344308 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,30 @@
+import sys, heapq
+
+input = sys.stdin.readline
+
+# 인접 리스트 생성
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+# 프림 알고리즘 진행
+hq = [(0, 1)]
+cnt = 0
+visit = [False] * (n + 1)
+maxDist, total = 0, 0
+while cnt != n:
+    curDist, curNode = heapq.heappop(hq)
+    if visit[curNode]:
+        continue
+
+    cnt += 1
+    visit[curNode] = True
+    maxDist = max(maxDist, curDist)
+    total += curDist
+    for toNode, toDist in graph[curNode]:
+        if not visit[toNode]:
+            heapq.heappush(hq, (toDist, toNode))
+print(total - maxDist)
