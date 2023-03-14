185da63edeb71a4652823468d4a0b980b0c8687d
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-03-15 08:55:37 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 85.10 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -0,0 +1,39 @@
+def tentotwo(number, ans):  # 십진수-> 이진수
+    if number == 0:
+        return ans
+    if number % 2 == 0:
+        return tentotwo(number // 2, "0" + ans)
+    elif number % 2 == 1:
+        return tentotwo(number // 2, "1" + ans)
+
+
+def adjustnumber(number):
+    temp = 0
+    for i in range(51):
+        temp = 2**i-1
+        if temp>=len(number):
+            break
+    return '0'*(temp-len(number))+number
+
+
+def check(number,parent):
+    if parent == '0': #만약에 루트 노드가 0이면 모든 child node는 0이어야 한다.
+        for child in number:
+            if child != '0':
+                return False
+    if len(number)==1: # 하나 밖에 없을때는 무조건 통과
+        return True
+    center = len(number)//2
+    return check(number[:center],number[center]) and check(number[center+1:],number[len(number)-center-1])
+
+
+def solution(numbers):
+    answer = []
+    for number in numbers:
+        temp = tentotwo(number, "")
+        temp = adjustnumber(temp)
+        if check(temp,temp[len(temp)//2]):
+            answer.append(1)
+        else:
+            answer.append(0)
+    return answer
