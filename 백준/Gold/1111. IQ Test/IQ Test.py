0fb5194e838bf25785d360d710da7764b294613d
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-03-27 08:40:36 +0900

    [Gold III] Title: IQ Test, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1111. IQ Test/IQ Test.py
@@ -0,0 +1,22 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+if n == 1:
+    print("A")
+elif n == 2:
+    if numbers[1] == numbers[0]:
+        print(numbers[0])
+    else:
+        print("A")
+else:
+    if numbers[0]==numbers[1]:
+        a=0
+    else:
+        a = (numbers[1] - numbers[2]) // (numbers[0] - numbers[1])
+    b = numbers[1] - numbers[0] * a
+
+    for i in range(n - 1):
+        expect = numbers[i] * a + b
+        if (numbers[i + 1] != expect):
+            print("B")
+            exit()
+    print(numbers[-1] * a + b)
