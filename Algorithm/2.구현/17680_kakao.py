e5d7434c988cbed1f9dff77feff0ec5f0a6386bc
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-03-29 22:54:32 +0900

    Create 17680_kakao.py

---
+++ Algorithm/2.구현/17680_kakao.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+
+def solution(cacheSize, cities):
+    answer = 0
+    cache = deque('' for _ in range(cacheSize))
+    for city in cities:
+        city = city.lower()
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            answer += 1
+        else:
+            flag = True
+            for i in range(len(cache)):
+                if cache[i] == '':
+                    cache[i] = city
+                    answer += 5
+                    flag = False
+                    break
+                else:
+                    continue
+            if flag:
+                answer += 5
+                if len(cache) > 0:
+                    cache.popleft()
+                    cache.append(city)
+    return answer
+
+
+# print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+print(solution(5, ["a", "b", "c", "a"]))
