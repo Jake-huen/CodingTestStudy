4c41faa564164e27f5fa9e2fed663984379e509d
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-04-24 13:35:11 +0900

    [Silver I] Title: 개미, Time: 296 ms, Memory: 37056 KB -BaekjoonHub

--- 백준/Silver/4307. 개미/개미.py
+++ 백준/Silver/4307. 개미/개미.py
@@ -1,9 +1,8 @@
 def fast(s, n):
-    mid = s // 2
     temp = []
     for i in range(n):
-        temp.append(abs(ants[i] - mid))
-    return mid - min(temp)
+        temp.append(min(ants[i], s - ants[i]))
+    return max(temp)
 
 
 def slow(s, n):
@@ -12,18 +11,17 @@ def slow(s, n):
         temp.append(min(s - ants[i], ants[i]))
     return s - min(temp)
 
+
 import sys
+
 input = sys.stdin.readline
 t = int(input())
 answer = []
 for _ in range(t):
     s, n = map(int, input().split())  # 막대의 길이, 개미의 수
     ants = []
-    min_time = 0
-    max_time = 0
     for i in range(n):
         ants.append(int(input()))
-    for ant in ants:
-        min_time = max(min_time, min(ant, s - ant))
-        max_time = max(max_time, ant, s - ant)
+    min_time = fast(s, n)
+    max_time = slow(s, n)
     print(min_time, max_time)
