00bcda620ac8e5d84d0a9537cae92446a6497db4
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-05-04 18:53:05 +0900

    5.04 5월 1주차 스터디

--- Algorithm/AMIALGORANI/5.04/1263.py
+++ Algorithm/AMIALGORANI/5.04/1263.py
@@ -6,19 +6,15 @@ for i in range(n):
     x, y = map(int, input().split())
     t.append(x)
     s.append(y)
-    p.append((y - x, y, x))
-p.sort(key=lambda x: (x[0], x[1]))
-start = p[0][0]
-if start < 0:
-    print(-1)
-else:
-    while start >= 0:
-        temp = start
-        for i in range(len(p)):
-            temp += p[i][2]
-            if temp > 24:
-                start -= 1
-                break
-        if temp < 24:
-            print(start)
-            break
+    p.append((x, y))
+p.sort(key=lambda x: (x[1]))
+time = 0
+while 1:
+    temp = time
+    for t, s in p:
+        if temp + t <= s:
+            temp += t
+        else:
+            print(time - 1) # 현재 이 시작시간이 안되는 거니까
+            exit()
+    time += 1
--- Algorithm/AMIALGORANI/5.04/17299.py
+++ Algorithm/AMIALGORANI/5.04/17299.py
@@ -1,40 +1,24 @@
-from collections import deque
+import sys
+
+input = sys.stdin.readline
 
 n = int(input())
 numbers = list(map(int, input().split()))
 f = {}
-stack = [] # stack에는 해당하는 숫자를 넣어준다
+stack = []  # stack에는 해당하는 숫자를 넣어준다
 for number in numbers:
     if number in f.keys():
         f[number] += 1
     else:
         f[number] = 1
 
-numbers = numbers[::-1]
-ngf = []
-
-for number in numbers:
-    if len(stack) == 0:  # 젤 처음 원소일때
-        stack.append(number)
-        ngf.append(-1)
-    else:
-        check = f[number] # 해당 숫자의 빈도 수
-        q = []
-        while stack:
-            tt = stack.pop()
-            check_in_stack = f[tt]
-            q.append(tt)
-            if check_in_stack > check:  # 오른쪽에 있으면서 더 큰 수
-                ngf.append(tt)
-                while q:
-                    stack.append(q.pop())
-                stack.append(number)
-                break
-        if len(stack) == 0:
-            while q:
-                stack.append(q.pop())
-            stack.append(number)
-            ngf.append(-1)
-print(*ngf[::-1])
+# numbers = numbers[::-1]
+ngf = [-1] * n
+stack = [0]
+for i in range(1, n):
+    while stack and f[numbers[stack[-1]]] < f[numbers[i]]:
+        ngf[stack.pop()] = numbers[i]
+    stack.append(i)
+print(*ngf)
 
 
