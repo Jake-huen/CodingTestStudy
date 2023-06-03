42248a1398be0411ad2413c7594d7961cc4d0122
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-06-03 17:40:50 +0900

    친구비 16562
    https://jobdong7757.tistory.com/208

--- Algorithm/AMIALGORANI/5.29/16562.py
+++ Algorithm/AMIALGORANI/5.29/16562.py
@@ -1,8 +1,7 @@
-from itertools import combinations
 from collections import deque
 
-n, m, k = map(int, input().split())
-costs = list(map(int, input().split()))
+n, m, k = map(int, input().split())  # 학생 수, 친구관계 수, 가지고 있는 돈
+costs = list(map(int, input().split()))  # 친구별 필요한 돈
 graph = [[] for _ in range(n)]
 
 for _ in range(m):
@@ -11,35 +10,28 @@ for _ in range(m):
     graph[w - 1].append(v - 1)
 temp = [i for i in range(n)]
 answers = []
+visited = [False for _ in range(n)]
 
-
-def friend_friend(nodes):
+def bfs(start):
     q = deque()
-    answer = 0
-    visited = [False for _ in range(n)]
-    for node in nodes:
-        q.append(node)
-        answer += costs[node]
+    members = set()
+    min_node = 10000
+    q.append(start)
     while q:
-        start = q.popleft()
-        visited[start] = True
-        for i in graph[start]:
-            if not visited[i]:
-                q.append(i)
-    return visited, answer
-
-
-for i in range(1, n + 1):
-    ways = list(combinations(temp, i))  # 친구를 고르는 모든 가지 수
-    for way in ways:
-        visited, answer = friend_friend(way)
-        flag = True
-        for k in range(n):  # 다 방문했는지 확인
-            if not visited[k]:
-                flag = False
-        if flag:
-            answers.append(answer)
-if len(answers) == 0 or min(answers) > k:
+        x = q.popleft()
+        if costs[x] < min_node:
+            min_node = costs[x]
+        visited[x] = True
+        members.add(x)
+        for node in graph[x]:
+            if not visited[node]:
+                q.append(node)
+    return min_node
+min_cost = 0
+for i in range(n):
+    if not visited[i]:
+        min_cost += bfs(i)
+if min_cost > k:
     print("Oh no")
 else:
-    print(min(answers))
+    print(min_cost)
