122c54b53b3be86bc1ba1ea1ae04fc901cf162a8
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-07-02 17:52:45 +0900

    7/1 study

--- Algorithm/AMIALGORANI/7.7/1528.py
+++ Algorithm/AMIALGORANI/7.7/1528.py
@@ -1,3 +1,24 @@
 n = int(input())
-for i in range(n+1):
-    if i
+if n <= 7:
+    d = [-1] * 8
+    d[4] = 1
+    d[7] = 1
+else:
+    d = [-1] * (n + 1)
+    d[4] = 1
+    d[7] = 1
+    for i in range(7, n + 1):
+        case1, case2 = 0, 0
+        if d[i - 4] >= 1:
+            case1 = d[i - 4] + 1
+        if d[i - 7] >= 1:
+            case2 = d[i - 7] + 1
+        if case1 ==0 and case2==0:
+            d[i] = -1
+        elif case1 == 0 and case2 != 0:
+            d[i] = case2
+        elif case1 != 0 and case2 == 0:
+            d[i] = case1
+        else:
+            d[i] = min(case1, case2)
+print(d[n])
