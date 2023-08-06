d3aa42c5e2237e223d4343c1578a06dc2046fadf
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-08-07 01:47:07 +0900

    1865 해결

--- Algorithm/AMIALGORANI/8.11/1865.py
+++ Algorithm/AMIALGORANI/8.11/1865.py
@@ -1,34 +1,39 @@
 import sys
+
 input = sys.stdin.readline
 
 tc = int(input())
-INF = 100001
 for _ in range(tc):
+
     n, m, w = map(int, input().split())
-    graph = [[INF] * (n + 1) for _ in range(n + 1)]
-    wormhole = [[INF] * (n + 1) for _ in range(n + 1)]
-    for a in range(1, n + 1):
-        for b in range(1, n + 1):
-            if a == b:
-                graph[a][b] = 0
+    edges = [[] for _ in range(n+1)]
+    distance = [10001] * (n + 1)
     for _ in range(m):
         s, e, t = map(int, input().split())
-        graph[s][e] = t
-        graph[e][s] = t
+        edges[s].append([e, t])
+        edges[e].append([s, t])
+
     for _ in range(w):
         s, e, t = map(int, input().split())
-        wormhole[s][e] = -t
-    for k in range(1, n + 1):
-        for a in range(1, n + 1):
-            for b in range(1, n + 1):
-                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b], graph[a][k] + wormhole[k][b],
-                                  wormhole[a][k] + graph[k][b], wormhole[a][k] + wormhole[k][b])
-    flag = False
-    for a in range(1, n + 1):
-        for b in range(1, n + 1):
-            if a == b and graph[a][b] < 0:
-                flag = True
-    if flag:
+        edges[s].append([e, -t])
+
+
+    def bellman_ford(start):
+        distance[start] = 0
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                for k in edges[j]:
+                    next_node = k[0]
+                    cost = k[1]
+                    if distance[next_node] > distance[j] + cost:
+                        distance[next_node] = distance[j] + cost
+                        if i == n:
+                            return True
+        return False
+
+
+    negative_cycle = bellman_ford(1)
+    if negative_cycle:
         print("YES")
     else:
         print("NO")
