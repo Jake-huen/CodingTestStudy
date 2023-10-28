18f1a61c00368b07d8f01bf0e9a7861cd4d2ee7b
username <57055730+Jake-huen@users.noreply.github.com>
2023-10-28 12:57:07 +0900

    Mentoring

--- Algorithm/AlgorithmJava/src/Mentoring.java
+++ Algorithm/AlgorithmJava/src/Mentoring.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Mentoring {
@@ -14,6 +17,33 @@ public class Mentoring {
                 graph[i][j] = kb.nextInt();
             }
         }
+        System.out.println(T.Solution(n, m, graph));
+    }
 
+    private int Solution(int n, int m, int[][] graph) {
+        int answers = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) { // 총 경우의 수
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    int first = 0;
+                    int second = 0;
+                    for (int l = 0; l < n; l++) {
+                        if (graph[k][l] == i) {
+                            first = l;
+                        } else if (graph[k][l] == j) {
+                            second = l;
+                        }
+                    }
+                    if (first < second) { // 뒤에 있는 경우
+                        cnt += 1;
+                    }
+                }
+                if (cnt == m) {
+                    answers += 1;
+                }
+            }
+        }
+        return answers;
     }
 }
--- Algorithm/daily/10.16/14891.py
+++ Algorithm/daily/10.16/14891.py
@@ -1,21 +1,93 @@
 # 톱니바퀴 N극은 0, S극은 1
-one = list(input())
-two = list(input())
-three = list(input())
-four = list(input())
+one = list(map(int, input()))
+two = list(map(int, input()))
+three = list(map(int, input()))
+four = list(map(int, input()))
+
+topni = []
+topni.append(one)
+topni.append(two)
+topni.append(three)
+topni.append(four)
 k = int(input())  # 회전시키는 방법
 
 
 def turn_left(line):  # 시계 반대방향으로 회전
-    return line[1:0]
+    # 1번 2번 3번 .... 0번
+    new_line = []
+    for i in range(1, len(line)):
+        new_line.append(line[i])
+    new_line.append(line[0])
+    return new_line
 
 
 def turn_right(line):  # 시계 방향으로 회전
-    return line[-1:-2]
+    # -1번 0번 1번 ...
+    new_line = []
+    for i in range(-1, len(line) - 1):
+        new_line.append(line[i])
+    return new_line
 
 
-
+score = 0
 for _ in range(k):
     num, dir = map(int, input().split())  # 회전시키는 번호, 정수 방향
-    # 체크해야하는건 오 왼오 왼오 왼
-    print(turn_left(one))
+    if num == 1:
+        temp = [dir, 0, 0, 0]
+        if topni[0][2] != topni[1][-2]:
+            temp[1] = -temp[0]
+            if topni[1][2] != topni[2][-2]:
+                temp[2] = -temp[1]
+                if topni[2][2] != topni[3][-2]:
+                    temp[3] = -temp[2]
+        for i in range(len(temp)):
+            if temp[i] == -1:
+                topni[i] = turn_left(topni[i])
+            elif temp[i] == 1:
+                topni[i] = turn_right(topni[i])
+    elif num == 2:
+        temp = [0, dir, 0, 0]
+        if topni[1][2] != topni[2][-2]:
+            temp[2] = -temp[1]
+            if topni[2][2] != topni[3][-2]:
+                temp[3] = -temp[2]
+        if topni[1][-2] != topni[0][2]:
+            temp[0] = -temp[1]
+        for i in range(len(temp)):
+            if temp[i] == -1:
+                topni[i] = turn_left(topni[i])
+            elif temp[i] == 1:
+                topni[i] = turn_right(topni[i])
+
+    elif num == 3:
+        temp = [0, 0, dir, 0]
+        if topni[2][2] != topni[3][-2]:
+            temp[3] = -temp[2]
+        if topni[2][-2] != topni[1][2]:
+            temp[1] = -temp[2]
+            if topni[0][2] != topni[1][-2]:
+                temp[0] = -temp[1]
+        for i in range(len(temp)):
+            if temp[i] == -1:
+                topni[i] = turn_left(topni[i])
+            elif temp[i] == 1:
+                topni[i] = turn_right(topni[i])
+
+    elif num == 4:
+        temp = [0, 0, 0, dir]
+        if topni[2][2] != topni[3][-2]:
+            temp[2] = -temp[3]
+            if topni[1][2] != topni[2][-2]:
+                temp[1] = -temp[2]
+                if topni[0][2] != topni[1][-2]:
+                    temp[0] = -temp[1]
+        for i in range(len(temp)):
+            if temp[i] == -1:
+                topni[i] = turn_left(topni[i])
+            elif temp[i] == 1:
+                topni[i] = turn_right(topni[i])
+
+for i in range(4):
+    if topni[i][0] == 1:
+        score += 2 ** i
+print(score)
