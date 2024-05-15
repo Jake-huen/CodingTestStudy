5f6790e9e0eb3b504a40acd9a039bd7207e08b05
username <57055730+Jake-huen@users.noreply.github.com>
2024-05-15 18:37:10 +0900

    BJ 서강그라운드
    플로이드-워셜

--- Algorithm/.BJPG/BJ14938/BJ서강그라운드.py
+++ Algorithm/.BJPG/BJ14938/BJ서강그라운드.py
@@ -1,7 +1,38 @@
 """
-낙하산에서 떨어질 때 각 지역에 아이템들이 몇 개 있는지 알려주는 프로그램
+자기 지역 n이 주어졌을 때, n에서 각 지역에 거리를 알아야함.
 """
-n, m, r = map(int, input().split())
+
+n, m, r = map(int, input().split())  # 지역 개수, 수색 범위, 길의 개수
 t = list(map(int, input().split()))
+item_graph = [[] for _ in range(n)]
+
+distance = [[10000001 for _ in range(n)] for _ in range(n)]
+
+# 본인 자신은 거리 0
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            distance[i][j] = 0
+            distance[j][i] = 0
+
 for _ in range(r):
-    a, b, l = map(int, input().split())
+    a, b, l = map(int, input().split())  # 지역번호, 지역번호, 길의 길이
+    item_graph[a - 1].append([b - 1, l])
+    item_graph[b - 1].append([a - 1, l])
+    distance[a - 1][b - 1] = l
+    distance[b - 1][a - 1] = l
+
+for i in range(n):  # 경유지
+    for j in range(n):  # 출발지
+        for k in range(n):  # 도착지
+            if distance[j][i] + distance[i][k] < distance[j][k]:
+                distance[j][k] = distance[j][i] + distance[i][k]
+# print(distance)
+result = 0
+for i in range(n):
+    ans = 0
+    for j in range(n):
+        if distance[i][j] <= m:
+            ans += t[j]
+    result = max(result, ans)
+print(result)
