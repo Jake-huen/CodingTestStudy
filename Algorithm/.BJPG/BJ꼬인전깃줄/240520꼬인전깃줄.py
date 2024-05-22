99b13b22e17dce7b25b8004a27361dc552550640
Jake-huen <57055730+Jake-huen@users.noreply.github.com>
2024-05-22 18:42:00 +0900

    Update 240520꼬인전깃줄.py

--- Algorithm/.BJPG/BJ꼬인전깃줄/240520꼬인전깃줄.py
+++ Algorithm/.BJPG/BJ꼬인전깃줄/240520꼬인전깃줄.py
@@ -1,22 +1,15 @@
+from bisect import bisect_left
+
 n = int(input())
 lines = list(map(int, input().split()))
-dp = [0] * n
-key = lines[0]
-for i in range(1, n):
-    if lines[i] > key:
-        key = lines[i]
-        dp[i] = dp[i - 1] + 1
+dp = [1]
+x = [lines[0]]
+
+for i in range(1, len(lines)):
+    if lines[i] > x[-1]:
+        x.append(lines[i])
+        dp.append(dp[-1] + 1)
     else:
-        key = lines[i]
-        dp[i] = 0
-result = n - max(dp) - 1
-dp = [0] * n
-for i in range(1, n):
-    if lines[i] < key:
-        key = lines[i]
-        dp[i] = dp[i - 1] + 1
-    else:
-        key = lines[i]
-        dp[i] = 0
-result = min(result, n - max(dp) - 1)
-print(result)
+        idx = bisect_left(x, lines[i])
+        x[idx] = lines[i]
+print(n - max(dp))
