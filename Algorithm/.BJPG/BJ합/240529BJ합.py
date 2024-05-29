ccca44afaaad8188089e9b9f15052e19e648417f
Jake-huen <57055730+Jake-huen@users.noreply.github.com>
2024-05-29 22:43:56 +0900

    합 수정

--- Algorithm/.BJPG/BJ합/240529BJ합.py
+++ Algorithm/.BJPG/BJ합/240529BJ합.py
@@ -1,19 +1,44 @@
 n = int(input())
 dict = {}
+inputs = []
 for _ in range(n):
     temp = list(input())
+    inputs.append(temp)
     m = 1
     for i in range(len(temp) - 1, -1, -1):
         if temp[i] in dict.keys():
             if i == 0:  # 첫번째 자리인 경우
-                dict[i][0] = False  # 0이 올 수 없음
-                dict[i][1] += m
+                dict[temp[i]][0] = False  # 0이 올 수 없음
+                dict[temp[i]][1] += m
             else:
-                dict[i][1] += m
+                dict[temp[i]][1] += m
         else:
             if i == 0:
-                dict[i] = [False, m]
+                dict[temp[i]] = [False, m]
             else:
-                dict[i] = [True, m]
+                dict[temp[i]] = [True, m]
         m *= 10
-print(dict)
+sorted_dict = sorted(dict.items(), key=lambda x: x[1], reverse=True)
+assignments = {}
+
+non_zero_candidates = [item[0] for item in sorted_dict if not item[1][0]]
+
+current_number = 9
+
+
+for candidate in non_zero_candidates:
+    assignments[candidate] = current_number
+    current_number -= 1
+
+for char, values in sorted_dict:
+    if char not in assignments:
+        assignments[char] = current_number
+        current_number -= 1
+print(assignments)
+total = 0
+for input in inputs:
+    m = 1
+    for i in range(len(input) - 1, -1, -1):
+        total += assignments[input[i]] * m
+        m *= 10
+print(total)
