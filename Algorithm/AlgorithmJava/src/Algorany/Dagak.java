2e8bcf580c455b3431a345138e648ac1109679e2
Jake-huen <57055730+Jake-huen@users.noreply.github.com>
2024-07-31 16:39:30 +0900

    수정 다각형 만들기

--- Algorithm/AlgorithmJava/src/Algorany/Dagak.java
+++ Algorithm/AlgorithmJava/src/Algorany/Dagak.java
@@ -11,18 +11,20 @@ public class Dagak {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         n = Integer.parseInt(br.readLine());
         x = new long[n + 1];
         y = new long[n + 1];
-        for (int i = 0; i < 4; i++) {
+        long a = 0;
+        long b = 0;
+        for (int i = 0; i < n; i++) {
             StringTokenizer str = new StringTokenizer(br.readLine());
             x[i] = Integer.parseInt(str.nextToken());
             y[i] = Integer.parseInt(str.nextToken());
         }
         x[n] = x[0];
         y[n] = y[0];
-        long a = 0;
-        long b = 0;
+
         for (int i = 0; i < n; i++) {
             a += (y[i + 1] * x[i]);
             b += (y[i] * x[i + 1]);
