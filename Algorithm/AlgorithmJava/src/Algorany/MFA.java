9bdbf5af466b2b950f788319af9c3d582f1d8da3
Jake-huen <57055730+Jake-huen@users.noreply.github.com>
2024-09-20 17:58:51 +0900

    9/20 색깔트리 코드 수정

--- Algorithm/AlgorithmJava/src/Algorany/MFA.java
+++ Algorithm/AlgorithmJava/src/Algorany/MFA.java
@@ -7,6 +7,7 @@ public class MFA {
     static StringTokenizer str;
     static int R, C, K;
     static int[][] graph;
+    static boolean[][] isExit;
     // 0북 1동 2남 3서
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
@@ -19,7 +20,8 @@ public class MFA {
         R = Integer.parseInt(str.nextToken());
         K = Integer.parseInt(str.nextToken()); // 정령 수
 
-        graph = new int[C + 1][R + 1];
+        graph = new int[C + 3][R + 1];
+        isExit = new boolean[C + 3][R + 1];
         for (int i = 1; i <= K; i++) {
             str = new StringTokenizer(br.readLine());
             int row = Integer.parseInt(str.nextToken()); // 출발하는 행
@@ -31,8 +33,18 @@ public class MFA {
         System.out.println(answer);
     }
 
+    public static boolean canGo(int col, int row) {
+        return false;
+    }
 
-
+    public static void down(int col, int row, int dir, int id) {
+        
+    }
+    
+    public static boolean inRange(int col, int row){
+        return false;
+    }
+    
     private static int moveGolam(int col, int row, int dir, int id) {
         if (canGo(col + 1, row)) {
             down(col + 1, row, dir, id);
@@ -45,7 +57,18 @@ public class MFA {
                 resetMap();
             } else {
                 // 정착
+                graph[col][row] = id;
+                for (int k = 0; k < 4; k++) {
+                    graph[col + dx[k]][row + dy[k]] = id;
+                }
+                isExit[col + dx[dir]][row + dy[dir]] = true;
+                answer += bfs(col, row) - 3 + 1;
             }
         }
     }
+
+    private static void resetMap() {
+    }
+
+
 }
