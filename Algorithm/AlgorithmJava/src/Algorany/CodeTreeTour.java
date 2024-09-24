683dabc1b878fb47cfa06d0197b46b0deca96522
Jake-huen <57055730+Jake-huen@users.noreply.github.com>
2024-09-24 21:00:09 +0900

    9/24 [fix] 코드트리여행 수정

--- Algorithm/AlgorithmJava/src/Algorany/CodeTreeTour.java
+++ Algorithm/AlgorithmJava/src/Algorany/CodeTreeTour.java
@@ -19,8 +19,8 @@ public class CodeTreeTour {
         for (int i = 0; i < Q; i++) {
             String[] order = br.readLine().split(" ");
             if (order[0].equals("100")) { // 랜드 건설
-                int n = Integer.parseInt(order[1]); // 도시 개수
-                int m = Integer.parseInt(order[2]); // 간선 개수
+                n = Integer.parseInt(order[1]); // 도시 개수
+                m = Integer.parseInt(order[2]); // 간선 개수
                 graph = new int[n][n];
                 for (int x = 0; x < n; x++) {
                     for (int y = 0; y < n; y++) {
@@ -28,9 +28,9 @@ public class CodeTreeTour {
                     }
                 }
                 for (int j = 0; j < m; j++) {
-                    int v = Integer.parseInt(order[2 + 3 * j]);
-                    int u = Integer.parseInt(order[3 + 3 * j]);
-                    int w = Integer.parseInt(order[4 + 3 * j]);
+                    int v = Integer.parseInt(order[3 + 3 * j]);
+                    int u = Integer.parseInt(order[4 + 3 * j]);
+                    int w = Integer.parseInt(order[5 + 3 * j]);
                     graph[v][u] = Math.min(graph[v][u], w);
                     graph[u][v] = Math.min(graph[u][v], w);
                 }
@@ -45,12 +45,19 @@ public class CodeTreeTour {
                 int id = Integer.parseInt(order[1]);
                 products.remove(id);
             } else if (order[0].equals("400")) { // 최적의 여행 상품 판매
-                int result = 0;
+                int profit = -1;
                 int idx = -1;
                 for (Map.Entry<Integer, int[]> integerEntry : products.entrySet()) {
-                    if (bestProduct(integerEntry.getValue()) > result) {
+                    int result = bestProduct(integerEntry.getValue());
+                    System.out.println("key = " + integerEntry.getKey() + " value = " + result);
+                    // System.out.println("result = " + result);
+                    if (result > profit) {
+                        profit = result;
                         idx = integerEntry.getKey();
                     }
+                }
+                if (idx != -1) {
+                    products.remove(idx);
                 }
                 System.out.println(idx);
             } else if (order[0].equals("500")) { // 여행 상품의 출발지를 전부 s로 변경
@@ -67,29 +74,31 @@ public class CodeTreeTour {
         // 이득이 음수라면 판매 불가 상품 / 도착지까지 갈 수 없다면 판매 불가 상품
         int rev = value[0];
         int dest = value[1];
+        //System.out.println("도착지 : " + dest);
         Queue<int[]> q = new LinkedList<>();
-        boolean[] visited = new boolean[n + 1];
+        boolean[] visited = new boolean[n];
         q.add(new int[]{s, 0});
+        //System.out.println("start = " + s);
         visited[s] = true;
-        int answer = Integer.MAX_VALUE;
+        int answer = -1;
         while (!q.isEmpty()) {
             int[] cur = q.poll();
+            int next = cur[0];
+            int cost = cur[1];
+            if (next == dest) {
+                System.out.println("dest = " + dest);
+                answer = Math.max(answer, rev - cost);
+            }
             for (int i = 0; i < n; i++) {
                 if (graph[cur[0]][i] != Integer.MAX_VALUE && !visited[i]) {
-                    if (i == dest) {
-                        answer = Math.min(answer, cur[1] + graph[cur[0]][i]);
-                        continue;
-                    }
                     visited[i] = true;
                     q.add(new int[]{i, cur[1] + graph[cur[0]][i]});
                 }
             }
         }
-        if (answer == Integer.MAX_VALUE) {
+        if (answer == -1) {
             return -1;
-        } else if(answer > rev){
-            return -1;
-        }else {
+        } else {
             return answer;
         }
     }
